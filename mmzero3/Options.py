459eba89064ee52f03d98df65e9545865522a98b
Brodie Berger <40439538+brodieberger@users.noreply.github.com>
2025-06-05 17:37:14 -0400

    Corrected win condition, added option for easy EX Skills. First alpha release!

--- mmzero3/Options.py
+++ mmzero3/Options.py
@@ -1,25 +1,29 @@
 from dataclasses import dataclass
-from Options import Choice, PerGameCommonOptions, Range
+from Options import Choice, Range, Toggle, PerGameCommonOptions
 
 class Goal(Choice):
     """
     Default: Beat the final boss with enough data disks collected to win.
     Vanilla: Beat the final boss to win.
-    Disk Hunt: Collect data disks to win.
     """
     display_name = "Goal"
     option_default = 0
     option_vanilla = 1
-    option_disk_hunt = 2
     default = 0
 
 class RequiredSecretDisks(Range):
     """Number of Secret Disks required to complete the game"""
     display_name = "Required Secret Disks"
     range_start = 0
-    range_end = 100
-    default = 60
+    range_end = 160
+    default = 80
+
+class EasyExSkill(Toggle):
+    """Give the player an S-Rank at the end of the level. Always gives EX-Skill. Note this can also make certain bosses harder, so don't consider this an easy mode!"""
+    display_name = "Always have S-Rank"
 
 @dataclass
 class MMZero3Options(PerGameCommonOptions):
     goal: Goal
+    required_secret_disks: RequiredSecretDisks
+    easy_ex_skill: EasyExSkill
